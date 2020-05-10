65ff7f3d1a360f556f756240a3181fd6afe31a7b
Alex Cherednyk <cherednyk.o@gmail.com>
2020-05-10 15:04:15 +0300

    update questions

--- app/constants.py
+++ app/constants.py
@@ -6,7 +6,8 @@ SCREEN_HEIGHT = 400
 
 # Welcome screen
 GREETINGS = (
-    "Hi!\n\nEmployee satisfaction is vital for us. \nWe use this quiz to collect the feedback on:"
+    "Hi!\n\nThis quiz was made to better understand employee satisfaction.\n"
+    "It'll be achieved by collecting the feedback on:"
     "\n - what is working in the organization well; \n - and what requires improvements. \n\n"
     "It will take you about 10 minutes to fill the survey in.\nWe appreciate your honesty and openness."
 )
@@ -14,7 +15,7 @@ GREETINGS = (
 # Questions screen
 SCORE_BTNS_AMOUNT = 10
 QUESTIONS = [
-    "How likely is it you would recommend ___ as a place to work?",
+    "How likely is it you would recommend your company as a place to work?",
     "At work, I have the opportunity to do what I do best every day.",
     "I can have well-informed and constructive conversations with my manager about pay.",
     "The atmosphere in my team is friendly and easy-going.",
